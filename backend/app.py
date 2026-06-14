b8b039840bbdfe364766f731a3a557bc37b1f91f
Mahesh <maheshseeda6@gmail.com>
2026-06-14 16:10:59 +0530

    Render deployment update

--- backend/app.py
+++ backend/app.py
@@ -51,5 +51,12 @@ def predict():
         "y": center_y
     })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
